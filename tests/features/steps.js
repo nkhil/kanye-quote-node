d96a68eef4ebfc464b8478e109155061ef16ccec
Nikhil Vijayan <nkhilv@gmail.com>
2019-06-10 18:58:21 +0100

    formatting

--- tests/features/steps.js
+++ tests/features/steps.js
@@ -1,29 +1,29 @@
-const { When, Then, And } = require("cucumber");
-const chai = require("chai");
-const rp = require("request-promise");
-const config = require("../config");
+const { When, Then, And } = require('cucumber');
+const chai = require('chai');
+const rp = require('request-promise');
+const config = require('../config');
 
 chai.should();
 
-When("I visit the {string}", async function(route) {
+When('I visit the {string}', async function(route) {
   this.response = await rp({
     url: `${config.thisService.url}`,
-    method: "GET",
-    resolveWithFullResponse: true
+    method: 'GET',
+    resolveWithFullResponse: true,
   });
 });
 
-Then("I will get a {int} response", function(status) {
+Then('I will get a {int} response', function(status) {
   this.response.statusCode.should.equal(status);
 });
 
-Then("The response body should be an Object", function() {
+Then('The response body should be an Object', function() {
   const body = JSON.parse(this.response.body);
-  body.should.be.an("object");
+  body.should.be.an('object');
 });
 
-Then("The response body should have length of one", function() {
+Then('The response body should have length of one', function() {
   const body = JSON.parse(this.response.body);
-  const length = Object.keys(body).length;
+  const {length} = Object.keys(body);
   length.should.equal(1);
 });
